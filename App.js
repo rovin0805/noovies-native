a94bd56f9ec6c6b50270acc7541e69891e6cc1a7
Choi Eun Jeong <qhdrn67@naver.com>
2021-11-16 00:59:48 +0900

    setting appLoading without hooks

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Image } from "react-native";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  // const [assets] = useAssets([require("./assets/quokka.jpeg")]);
+  // const [fonts] = Font.useFonts(Ionicons.font);
+  const [ready, setReady] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const loadFonts = (fontsArr) => fontsArr.map((font) => Font.loadAsync(font));
+  const loadImages = (imagesArr) =>
+    imagesArr.map((image) => {
+      if (typeof image === "string") {
+        return Image.prefetch(image);
+      } else {
+        return Asset.loadAsync(image);
+      }
+    });
+
+  const startLoading = async () => {
+    const fonts = loadFonts([Ionicons.font]);
+    const images = loadImages([
+      require("./assets/quokka.jpeg"),
+      "https://reactnative.dev/img/oss_logo.png",
+    ]);
+    await Promise.all([...fonts, ...images]);
+  };
+
+  const onFinish = () => setReady(true);
+
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
+  }
+
+  return null;
+}
