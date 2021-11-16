e81e4f138cf82256e7209c3b92daa1e8c171c06c
Choi Eun Jeong <qhdrn67@naver.com>
2021-11-16 17:39:14 +0900

    tab bar icon

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,14 +3,47 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies} />
-    <Tab.Screen name="Tv" component={Tv} />
-    <Tab.Screen name="Search" component={Search} />
+  <Tab.Navigator
+    screenOptions={{
+      tabBarLabelStyle: {
+        marginTop: -5,
+        fontSize: 10,
+        fontWeight: "600",
+      },
+    }}
+  >
+    <Tab.Screen
+      name="Movies"
+      component={Movies}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name={"film-outline"} color={color} size={size} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Tv"
+      component={Tv}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="tv-outline" color={color} size={size} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Search"
+      component={Search}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name={"search-outline"} color={color} size={size} />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 
